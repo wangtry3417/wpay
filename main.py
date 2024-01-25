928554418b343dd2e26074ee47f2f99598f58d4d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-25 09:39:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from flask import Flask,redirect,render_template,url_for
+import random
 
+app = Flask("WPay")
+
+@app.route("/wcoin")
+def wcoin():
+  return render_template("wcoin.html")
+
+
+
+app.run(host="0.0.0.0",port=5000)
